0c8e64240ad417135c648bd46954cb83876dd058
hanekaoru <vd2o@foxmail.com>
2017-11-18 23:20:48 +0800

    Update Common.js

--- Common.js
+++ Common.js
@@ -55,3 +55,117 @@ $.fn.extend({
         return $(this);
     }
 });
+
+
+
+
+$.extend({
+    showMsg: function (opts, other) {
+        var arrayHtml = [
+            '<div class="dialogTitle">' + (opts.title || '提示') + '<div class="d" style="">×</div></div>',
+            '<p style="line-height: 95px;text-align: center;font-size: 15px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;" class="txt">' + (opts.msg || '') + '</p>',
+            '<div id="dialogSure" class="btn btnprim fl newbtn" style="margin-top:0;padding: 0 20px;0px;margin-left: 78px;margin-right:15px;">' + (opts.yesText || '确定') + '</div>',
+            '<div id="dialogClose" class="btn btnprim cancel fl newbtn" style="margin-top:0;padding: 0 20px;0px;">' + (opts.noText || '取消') + '</div>'];
+        $.dialogJplus(Object.assign({
+            containerCss: {
+                width: '300px',
+                height: '190px',
+                'background-color': '#fff',
+                color: '#666',
+                "padding": "0px"
+            },
+            closeHTML: '',
+            arrayHtml: arrayHtml,
+            onShow: function () {
+                $('#simplemodal-container').css({
+                    'box-shadow': 'rgb(93, 93, 93) 0px 0px 20px'
+                });
+                opts.onShow && opts.onShow($('#simplemodal-container'));
+                $(".cancel,.d").click(function () {
+                    opts.onClose && opts.onClose();
+                    $.modal.close();
+                });
+                $("#dialogSure").click(function () {
+                    if ($(this).attr('sub') == '1') return false;
+                    $(this).attr('sub', 1).html('确定...');
+                    opts.onSure && opts.onSure($(this));
+                });
+            }
+        }, other));
+    },
+    alertSuc: function (str, fn, title) {
+        var arrayHtml = [
+            '<div class="dialogTitle">' + (title || '提示') + '<div class="d" style="">×</div></div>',
+            '<p style="line-height: 95px;text-align: center;font-size: 15px;color: #5AB317;" class="txt">' + str + '</p>'];
+        $.dialogJplus({
+            containerCss: {
+                width: '255px',
+                height: '160px',
+                'background-color': '#fff',
+                color: '#666',
+                "padding": "0px"
+            },
+            closeHTML: '',
+            arrayHtml: arrayHtml,
+            onShow: function () {
+                $('#simplemodal-container').css({
+                    'box-shadow': 'rgb(93, 93, 93) 0px 0px 20px'
+                });
+                $(".cancel,.d").click(function () {
+                    $.modal.close();
+                });
+                fn && fn($(this));
+            }
+        });
+
+    },
+    simpleLoading: function (str, fn) {
+        $('body').append([
+            '<div class="laymask" style="top:0;"></div>',
+            '<div class="loadng"><div class="delloading" style="display: none;">×</div><div class="loading fl" style="margin-right: 10px;"></div><div class="fl" style="font-size: 15px;margin-top: 3px;">' + (str || '正在打开...') + '</div></div>',
+        ].join(''));
+        setTimeout(function () {
+            $('.loadng').addClass('gaimcnor');
+        }, 5);
+        fn && fn();
+    },
+    remsimpleLoading: function () {
+        $('.laymask,.loadng').remove();
+    },
+    delReload: function (data, str1, str2) {
+        window.parent.$.modal.close();
+        if (data <= 0) {
+            window.parent.$.alert(str2 || '存在不能删除的，能删除的成功删除！');
+            setTimeout(function () {
+                location.reload();
+                window.parent.$.modal.close();
+            }, 1500);
+        }
+        if (data > 0) {
+            window.parent.$.alertSuc(str1 || '删除成功！');
+            setTimeout(function () {
+                location.reload();
+                window.parent.$.modal.close();
+            }, 1500);
+        }
+    },
+    saveReload: function (data, str1, str2) {
+        window.parent.$.modal.close();
+        if (data <= 0) {
+            window.parent.$.alert(str2 || '存在重复！');
+            setTimeout(function () {
+
+                window.parent.$.modal.close();
+                location.reload();
+            }, 1500);
+        }
+        if (data > 0) {
+            window.parent.$.alertSuc(str1 || '保存成功！');
+            setTimeout(function () {
+
+                window.parent.$.modal.close();
+                location.reload();
+            }, 1500);
+        }
+    }
+})
